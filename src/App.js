bc30739b909323bac015f7305e8a09b473211069
fabricio-fig <fabriciofgrd9@gmail.com>
2022-02-20 22:44:54 -0300

    estilizando com css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import {FiSearch} from 'react-icons/fi';
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <h1>Teste projeto</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+
+          <input type="text" placeholder="Digite seu cep..." />
+        
+          <button className="buttonSearch">
+            <FiSearch size={25} color="#FFF"/>
+          </button>
+
+      </div>
+      <main className="main">
+          <h2>CEP: 790322200</h2>
+        <span>Rua Teste</span>
+        <span>Complemento: Algum Complemento</span>
+        <span>Vila Rosa</span>
+        <span>Natal - RN</span>
+      </main>
+
     </div>
   );
 }
